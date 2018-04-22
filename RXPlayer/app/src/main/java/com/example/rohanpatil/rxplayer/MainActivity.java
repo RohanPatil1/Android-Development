3e31e1f9ed0f223d473f339b9d578150ba20dc58
Rohan Patil <prorohan8@gmail.com>
2018-04-22 20:38:08 +0530

    RX Player

---
+++ RXPlayer/app/src/main/java/com/example/rohanpatil/rxplayer/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.rohanpatil.rxplayer;
+
+
+
+
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.media.MediaPlayer;
+        import android.os.Handler;
+        import android.os.Message;
+        import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+        import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.ImageView;
+        import android.widget.SeekBar;
+        import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button playBtn;
+    SeekBar positionBar;
+
+    TextView elapsedTimeLabel;
+    TextView remainingTimeLabel;
+    MediaPlayer mp;
+    int totalTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView album = (ImageView) findViewById(R.id.album);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.album);
+       RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(),bitmap);
+       roundedBitmapDrawable.setCircular(true);
+       album.setImageDrawable(roundedBitmapDrawable);
+
+        playBtn = (Button) findViewById(R.id.playBtn);
+        elapsedTimeLabel = (TextView) findViewById(R.id.elapsedTimeLabel);
+        remainingTimeLabel = (TextView) findViewById(R.id.remainingTimeLabel);
+
+        // Media Player
+        mp = MediaPlayer.create(this, R.raw.music);
+        mp.setLooping(true);
+        mp.seekTo(0);
+
+        totalTime = mp.getDuration();
+
+        // Position Bar
+        positionBar = (SeekBar) findViewById(R.id.positionBar);
+        positionBar.setMax(totalTime);
+        positionBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if (fromUser) {
+                            mp.seekTo(progress);
+                            positionBar.setProgress(progress);
+                        }
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+
+        // Volume Bar
+
+
+        // Thread (Update positionBar & timeLabel)
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mp != null) {
+                    try {
+                        Message msg = new Message();
+                        msg.what = mp.getCurrentPosition();
+                        handler.sendMessage(msg);
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {}
+                }
+            }
+        }).start();
+
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int currentPosition = msg.what;
+            // Update positionBar.
+            positionBar.setProgress(currentPosition);
+
+            // Update Labels.
+            String elapsedTime = createTimeLabel(currentPosition);
+            elapsedTimeLabel.setText(elapsedTime);
+
+            String remainingTime = createTimeLabel(totalTime-currentPosition);
+            remainingTimeLabel.setText("- " + remainingTime);
+        }
+    };
+
+    public String createTimeLabel(int time) {
+        String timeLabel = "";
+        int min = time / 1000 / 60;
+        int sec = time / 1000 % 60;
+
+        timeLabel = min + ":";
+        if (sec < 10) timeLabel += "0";
+        timeLabel += sec;
+
+        return timeLabel;
+    }
+
+    public void playBtnClick(View view) {
+
+        if (!mp.isPlaying()) {
+            // Stopping
+            mp.start();
+            playBtn.setBackgroundResource(R.drawable.stop);
+
+        } else {
+            // Playing
+            mp.pause();
+            playBtn.setBackgroundResource(R.drawable.play);
+        }
+
+    }
+}
