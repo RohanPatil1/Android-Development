89aeb4d2de649818a6569a35bb0bb22bc1b53f75
Rohan Patil <prorohan8@gmail.com>
2018-06-07 21:36:01 +0530

    ScoreKeeper(Google Scholarship Project)
    Build for Google India Udacity Scholarship.

--- CourtCounterApp/app/src/main/java/com/example/android/courtcounterapp/MainActivity.java
+++ CourtCounterApp/app/src/main/java/com/example/android/courtcounterapp/MainActivity.java
@@ -14,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
     /**
      * Displays the given score for Team A.
      */
@@ -23,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreViewA = (TextView) findViewById(R.id.team_a_score);
         scoreViewA.setText(String.valueOf(score));
     }
-
     /**
      * Displays the given score for Team B.
      */
@@ -31,37 +28,30 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreViewB = (TextView) findViewById(R.id.team_b_score);
         scoreViewB.setText(String.valueOf(score));
     }
-
     public void plus3A(View va){
         scoreTeamA = scoreTeamA +3;
         displayForTeamA(scoreTeamA);
     }
-
     public void plus2A(View va){
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
-
     public void plus1A(View va){
         scoreTeamA = scoreTeamA +1;
         displayForTeamA(scoreTeamA);
     }
-
     public void plus3B(View v){
         scoreTeamB = scoreTeamB  +3;
         displayForTeamB(scoreTeamB );
     }
-
     public void plus2B(View v){
         scoreTeamB  = scoreTeamB  + 2;
         displayForTeamB(scoreTeamB );
     }
-
     public void plus1B(View v){
         scoreTeamB  = scoreTeamB  +1;
         displayForTeamB(scoreTeamB );
     }
-
     public void reset(View v){
         scoreTeamA = 0;
         scoreTeamB=0;
